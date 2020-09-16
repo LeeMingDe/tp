bd710607c8c1c4020f627b126b7efcf88aa86d54
Lee Ming De <leemingde@gmail.com>
2020-09-16 18:35:12 +0800

    Add new Command to AddressBook

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -13,6 +13,7 @@ import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Remark;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -78,6 +79,15 @@ public class ParserUtil {
             throw new ParseException(Address.MESSAGE_CONSTRAINTS);
         }
         return new Address(trimmedAddress);
+    }
+
+    public static Remark parseRemark(String address) throws ParseException {
+        requireNonNull(address);
+        String trimmedRemark = address.trim();
+        if (!Remark.isValidRemark(trimmedRemark)) {
+            throw new ParseException(Address.MESSAGE_CONSTRAINTS);
+        }
+        return new Remark(trimmedRemark);
     }
 
     /**
