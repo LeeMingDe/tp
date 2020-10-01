f3745baf93e257434ce0bfc9d60d36ba081bbb04
Lee Ming De <leemingde@gmail.com>
2020-10-01 23:59:56 +0800

    Fix refactor bugs

--- src/main/java/seedu/address/logic/commands/patient/ListPatientCommand.java
+++ src/main/java/seedu/address/logic/commands/patient/ListPatientCommand.java
@@ -8,13 +8,13 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists all patients in the address book to the user.
  */
 public class ListPatientCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all patients";
 
 
     @Override
